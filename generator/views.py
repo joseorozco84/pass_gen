0b613a0c54d8439135c3e5c4b764f85258b54bd0
joseorozco84 <joseorozco84@gmail.com>
2022-06-29 20:49:42 -0300

    comitea

---
+++ generator/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+    generated_password = ''
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('_-+!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    for _ in range(length):
+        generated_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': generated_password})
